5878a8f4c823c3a46f3540a9702d91b6f1f04fe5
Tatianeaj <tatiane-aj@hotmail.com>
2023-09-10 20:08:18 -0300

    clean_code

--- views/user_form.php
+++ views/user_form.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Usuários</title>
-    <!-- Inclua o arquivo CSS do Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 p-4">
