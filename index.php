458af8404899c8726ab4ce323d5b3f03e23eb3fa
Tatianeaj <tatiane-aj@hotmail.com>
2023-09-10 16:06:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link href="/dist/output.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>
+    
+    <?php 
+    
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'home';
+}
+    include "views/$page.php"
+    ?>
+</body>
+</html>
